8fc1aff69315e4f36e856a47fed8ecafd0746c89
Barton NIU <barton.niu@gmail.com>
2020-05-08 20:33:25 +1000

    Add card layout for search results

--- components/ResultsList.js
+++ components/ResultsList.js
@@ -2,6 +2,7 @@ import React, { useContext } from 'react';
 import { StyleSheet, Text, View, Image, FlatList, TouchableOpacity } from 'react-native';
 import { DataContext } from './DataContext';
 import Icon from '@expo/vector-icons/FontAwesome5';
+import { images } from '../utils/images';
 
 const ResultsList = ({ navigation }) => {
 
@@ -18,13 +19,14 @@ const ResultsList = ({ navigation }) => {
             <Text style={styles.text}>{items.length} results found and listed below </Text>
 
             <FlatList
-                // numColumns={2}
-                // horizontal={false}
+                numColumns={2}
+                horizontal={false}
                 data={items}
                 keyExtractor={(item) => item.id.toString()}
                 renderItem={({ item }) => {
                     return (
                         <TouchableOpacity style={styles.listItem} onPress={() => navigation.navigate('Details', item)} >
+                            <Image source={images.ids[item.id.toString().split("")[0]]} style={styles.itemImage} />
                             <View style={styles.listItemView}>
                                 <Text >{item.title}</Text>
                                 <Icon name="angle-right" size={20} color="#ddd" />
@@ -55,13 +57,18 @@ const styles = StyleSheet.create({
         fontSize: 13,
     },
     listItem: {
-        width: '100%',
+        width: '50%',
         padding: 10,
         backgroundColor: '#f8f8f8',
         borderBottomWidth: 1,
         borderColor: '#eee',
         marginBottom: 5,
 
+    },
+    itemImage: {
+        width: '100%',
+        height: 200,
+        marginBottom: 10,
     },
     listItemView: {
         flexDirection: 'row',
--- screens/Home.js
+++ screens/Home.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { StyleSheet, Text, View, Button } from 'react-native'
+import { StyleSheet, Text, View, Button, TouchableWithoutFeedback, Keyboard } from 'react-native'
 import SliderBanner from '../components/SliderBanner'
 import ResultsList from '../components/ResultsList'
 
@@ -8,16 +8,24 @@ const Home = ({ navigation }) => {
     console.log("home navigation is ", navigation);
 
     return (
-        <>
+        <TouchableWithoutFeedback
+            onPress={() => {
+                Keyboard.dismiss();
+                console.log("dismiss keyboard")
+            }
+            }>
+            <>
+                <SliderBanner />
+                <ResultsList navigation={navigation} />
+                {/* <Text>This is home page</Text> */}
+                <Button
+                    title="Go to videos page"
+                    onPress={() => navigation.navigate('Videos')}
+                />
+            </>
+        </TouchableWithoutFeedback >
 
-            <SliderBanner />
-            <ResultsList navigation={navigation} />
-            {/* <Text>This is home page</Text> */}
-            <Button
-                title="Go to videos page"
-                onPress={() => navigation.navigate('Videos')}
-            />
-        </>
+
     )
 }
 
